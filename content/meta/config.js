706844421dbdf6b1318a2a68c0ad5a6c468a45cd
Rkiguel Figueiro <riguel.figueiro@thoughtworks.com>
2020-04-09 13:45:45 -0300

    changed configurations

--- content/meta/config.js
+++ content/meta/config.js
@@ -1,26 +1,26 @@
 module.exports = {
-  siteTitle: "HeroBlog - a GatsbyJS starter", // <title>
-  shortSiteTitle: "HeroBlog - GatsbyJS starter", // <title> ending for posts and pages
-  siteDescription: "HeroBlog is a GatsbyJS starter.",
-  siteUrl: "https://gatsby-starter-hero-blog.greglobinski.com",
+  siteTitle: "Riguel's blog", // <title>
+  shortSiteTitle: "Riguel's blog", // <title> ending for posts and pages
+  siteDescription: "I’m a javascript developer. I love to share knowledge and discover new technologies",
+  siteUrl: "https://www.riguel.com.br",
   // pathPrefix: "",
   siteImage: "preview.jpg",
   siteLanguage: "en",
 
   /* author */
-  authorName: "greg lobinski",
-  authorTwitterAccount: "greglobinski",
+  authorName: "Riguel Figueiro",
+  authorTwitterAccount: "rbfz800",
 
   /* info */
-  headerTitle: "greg lobinski",
-  headerSubTitle: "presents another one GatsbyJS starter",
+  headerTitle: "Riguel Figueiro",
+  headerSubTitle: "Just a full stack Javascript developer",
 
   /* manifest.json */
-  manifestName: "HeroBlog - a GatsbyJS starter",
-  manifestShortName: "HeroBlog", // max 12 characters
+  manifestName: "Riguel's blog",
+  manifestShortName: "RiguelBlog", // max 12 characters
   manifestStartUrl: "/index.html",
-  manifestBackgroundColor: "white",
-  manifestThemeColor: "#666",
+  manifestBackgroundColor: "black",
+  manifestThemeColor: "#000",
   manifestDisplay: "standalone",
 
   // gravatar
@@ -32,8 +32,8 @@ module.exports = {
 
   // social
   authorSocialLinks: [
-    { name: "github", url: "https://github.com/greglobinski" },
-    { name: "twitter", url: "https://twitter.com/greglobinski" },
-    { name: "facebook", url: "http://facebook.com/greglobinski" }
+    { name: "github", url: "https://github.com/riguelbf" },
+    { name: "twitter", url: "https://twitter.com/rbfz800" },
+    { name: "facebook", url: "http://facebook.com/riguel.figueiro" }
   ]
 };
